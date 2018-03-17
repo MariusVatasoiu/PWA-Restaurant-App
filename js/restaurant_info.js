cf96f94c9eb8d8fef073057b7759a807dec15d66
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2018-03-17 23:48:06 +0200

    feat: add badge for ratings

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -126,7 +126,8 @@ createReviewHTML = (review) => {
   li.appendChild(date);
 
   const rating = document.createElement('p');
-  rating.innerHTML = `Rating: ${review.rating}`;
+	rating.innerHTML = `Rating: ${review.rating}`;
+	rating.classList.add('badge');
   li.appendChild(rating);
 
   const comments = document.createElement('p');
