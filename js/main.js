060803e52af46cd18601045670d0eda907c5cb4b
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2018-03-19 00:03:12 +0200

    feat: add service worker for cache

--- js/main.js
+++ js/main.js
@@ -9,7 +9,8 @@ var markers = []
  */
 document.addEventListener('DOMContentLoaded', (event) => {
   fetchNeighborhoods();
-  fetchCuisines();
+	fetchCuisines();
+	registerServiceWorker();
 });
 
 /**
@@ -193,3 +194,15 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 }
 
+registerServiceWorker = () => {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/sw.js')
+		.then(function(reg) {
+			// registration worked
+			console.log('Registration succeeded. Scope is ' + reg.scope);
+		}).catch(function(error) {
+			// registration failed
+			console.log('Registration failed with ' + error);
+		});
+	}
+}
