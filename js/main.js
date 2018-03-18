60513aaebfc08ae9c116e8561c6ad7b895bcf542
Marius Vatasoiu <marius.vatasoiu@outlook.com>
2018-03-19 00:09:26 +0200

    fix: change registration for sw

--- js/main.js
+++ js/main.js
@@ -196,13 +196,16 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 
 registerServiceWorker = () => {
 	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('/sw.js')
-		.then(function(reg) {
-			// registration worked
-			console.log('Registration succeeded. Scope is ' + reg.scope);
-		}).catch(function(error) {
-			// registration failed
-			console.log('Registration failed with ' + error);
+		navigator.serviceWorker.register('sw.js', {
+			scope: './'
 		});
+		// navigator.serviceWorker.register('/sw.js')
+		// .then(function(reg) {
+		// 	// registration worked
+		// 	console.log('Registration succeeded. Scope is ' + reg.scope);
+		// }).catch(function(error) {
+		// 	// registration failed
+		// 	console.log('Registration failed with ' + error);
+		// });
 	}
 }
