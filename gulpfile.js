b99fe9bdf2e72b578e7a43e54eb012ac529c6490
Marius Vatasoiu <marius.vatasoiu@oracle.com>
2018-04-04 14:17:20 +0300

    Add browser-sync and change public folder to dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function(){
+gulp.task('default',['copy-html', 'copy-images', 'styles'], function(){
   console.log('hello world');
+});
+
+gulp.task('watch', ['browserSync', 'styles'], function(){
+  gulp.watch('sass/**/*.scss', ['styles']);
+  gulp.watch('/index.html', ['copy-html']);
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('js/**/*.js', browserSync.reload);
+});
+
+gulp.task('copy-html', function(){
+  gulp.src('./index.html')
+      .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('copy-images', function(){
+  gulp.src('img/*')
+      .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('styles', function(){
@@ -12,5 +30,17 @@ gulp.task('styles', function(){
       .pipe(autoprefixer({
         browsers: ['last 2 versions']
       }))
-      .pipe(gulp.dest('./css'));
+      .pipe(gulp.dest('./dist/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+});
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    port: 8000,
+    server: {
+      baseDir: "./dist"
+    }
+  });
 });
